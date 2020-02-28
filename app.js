49c3071099ddb4cfb2a4663144aacf498944e3e0
Richard So <richard.so21@sitechhs.com>
2020-02-28 14:07:23 -0500

    reversing stuff cuz its too confusing to make

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-function wipes(){
-  const controller = new ScrollMagic.Controller;
+// function wipes(){
+//   const controller = new ScrollMagic.Controller;
 
-  var wipeAnimation = new TimelineMax()
-  // animate to second panel
-  // .to("#test", 0.5, {z: -150})		// move back in 3D space
-  // .to("#test", 1,   {x: "-25%"})	// move in to first panel
-  // .to("#test", 0.5, {z: 0})
-  .fromTo("section.block-culture", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
+//   var wipeAnimation = new TimelineMax()
+//   // animate to second panel
+//   // .to("#test", 0.5, {z: -150})		// move back in 3D space
+//   // .to("#test", 1,   {x: "-25%"})	// move in to first panel
+//   // .to("#test", 0.5, {z: 0})
+//   .fromTo("section.block-culture", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
 
-  new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.content-blocks',
-    triggerHook: 'onLeave'
-  })
-  .setPin('.block-why')
-  .setTween(wipeAnimation)
-  .addIndicators()
-  .addTo(controller)
-}
+//   new ScrollMagic.Scene({
+//     duration: '100%',
+//     triggerHook: 'onLeave',
+//     triggerElement: '.content-blocks'
+//   })
+//   .setPin('.content-blocks')
+//   .setTween(wipeAnimation)
+//   .addIndicators()
+//   .addTo(controller)
+// }
 
-wipes();
+// wipes();
