84905d23cf00add53587365791ef19142cac0b0f
Richard So <richard.so21@sitechhs.com>
2020-02-29 00:22:19 -0500

    yikes lol

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
-// function wipes(){
-//   const controller = new ScrollMagic.Controller;
-//   var wipeAnimation = new TimelineMax()
-//   // animate to second panel
-//   // .to("#test", 0.5, {z: -150})		// move back in 3D space
-//   // .to("#test", 1,   {x: "-25%"})	// move in to first panel
-//   // .to("#test", 0.5, {z: 0})
-//   .fromTo("section.block-culture", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
-//   new ScrollMagic.Scene({
-//     duration: '100%',
-//     triggerHook: 'onLeave',
-//     triggerElement: '.content-blocks'
-//   })
-//   .setPin('.content-blocks')
-//   .setTween(wipeAnimation)
-//   .addIndicators()
-//   .addTo(controller)
+// window.onload = function(){
+//   this.welcomefunction();
 // }
-// wipes();
-// =======================================
+// function welcomefunction() {
+//   // const welcome = document.querySelectorAll(".welcome-intro path");
+//   // // console.log(welcome);
+//   const welcome = document.querySelectorAll(".why-dynamic path");
+//   console.log(welcome);
+//   for (let i = 0; i < welcome.length; i++) {
+//     console.log(`Letter ${i} is ${welcome[i].getTotalLength()}`);
+//   }}
 
-window.onload = function(){
-  this.welcomefunction();
-}
-
-function welcomefunction() {
-  // const welcome = document.querySelectorAll(".welcome-intro path");
-  // // console.log(welcome);
-  const welcome = document.querySelectorAll(".why-path1");
-  console.log(welcome);
-  for (let i = 0; i < welcome.length; i++) {
-    console.log(`Letter ${i} is ${welcome[i].getTotalLength()}`);
-  }}
-
-// Letter 0 is 2000.1795654296875
-// Letter 1 is 3178.713134765625
+// Letter 0 is 422.51px
+// Letter 1 is 293.90px
+// Letter 2 is 216.07px
+// Letter 3 is 183.45px
+// Letter 4 is 254.51px
+// Letter 5 is 225.63px
+// Letter 6 is 254.51px
+// Letter 7 is 307.03px
+// Letter 8 is 220.42px
 
 // ===========================================
 
 // var controller = new ScrollMagic.Controller();
 
-// var tween = new TimelineMax()
-//   .add(TweenMax.to(".why-path1", 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
+// var t1 = new gsap.timeline()
+//   .to(".why-dynamic path", {strokeDashoffset: 0, duration: 1});
 
-// var scene = new ScrollMagic.Scene({triggerElement: ".content-blocks", duration: 200, tweenChanges: true, triggerHook: 1})
-//           .setTween(tween)
-//           .addIndicators() // add indicators (requires plugin)
+// var scene = new ScrollMagic.Scene({triggerElement: ".content-blocks", duration: "75%", tweenChanges: true, triggerHook: 1})
+//           .setTween(t1)
+//           .addIndicators() 
 //           .addTo(controller);
 
+// ==========================================
